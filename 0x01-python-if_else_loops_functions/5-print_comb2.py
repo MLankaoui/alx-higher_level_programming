c8fd2320446d566942d9fb9c71a0ed8aae0e84cb
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-13 00:20:35 +0100

    task 5 quick update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number < 10:
-        print("0", end="")
+        print("{:d}".format(0) ,end="")
 
-    print(number)
+    print("{number}".format(number=number))

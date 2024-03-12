49a5424c1747a49e3aa030f386956f9e33e272e3
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-13 00:17:13 +0100

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-for deci in range(0, 10) + units in range(0, 10):
-        if deci != 9 or units != 9:
-            print("{deci}{units}, ".format(deci=deci, units=units), end="")
-        elif deci == 9 and units == 9:
-            print("{deci}{units}".format(deci=deci, units=units))
+for number in range(0, 100):
+    if number < 10:
+        print("0", end="")
+        print(number)
+    else:
+        print(number)

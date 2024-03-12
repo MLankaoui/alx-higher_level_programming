2cd3944bc11ef3407edbbb85d42978c16d3a15db
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-12 23:07:30 +0100

    task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last = number % -10
+        last = last * -1
+        print(last, end="")
+        return last
+    
+    else:
+        last = number % 10
+        print(last, end="")
+        return last

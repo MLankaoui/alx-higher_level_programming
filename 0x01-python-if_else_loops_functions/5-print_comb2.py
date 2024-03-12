6b667777c4921b03ba9a071193d21f53aca4848d
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-13 00:28:12 +0100

    pycodestyle task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number < 10:
-        print("{:d}".format(0) ,end="")
+    if number < 99:
+        if number < 10:
+            print("{:d}".format(0), end="")
+        print("{number}, ".format(number=number), end="")
 
-    print("{number}".format(number=number))
+    else:
+        number += 1

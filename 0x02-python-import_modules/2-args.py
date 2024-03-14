324ad618f4691b21db66b364f84f97d264317173
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-14 09:12:08 +0000

    task another attempt

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,20 @@
-import sys
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-argc = len(sys.argv) - 1
+    argc = len(sys.argv) - 1
 
-i = 1
-if argc == 1:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
+    i = 1
+    if argc == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
 
-elif argc > 1:
-    print("{} arguments:".format(argc))
+    elif argc > 1:
+        print("{} arguments:".format(argc))
 
-    for argument in sys.argv[1:]:
-        print("{}: {}".format(i, sys.argv[i]))
-        i += 1
+        for argument in sys.argv[1:]:
+            print("{}: {}".format(i, sys.argv[i]))
+            i += 1
 
-else:
-    print(".")
+    else:
+        print(".")

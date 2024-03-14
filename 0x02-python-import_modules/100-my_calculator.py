4fee729d730591bc342efe2db4348ee6132f6858
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-14 10:42:29 +0000

    pycode style for task 100 fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,24 +13,24 @@ if __name__ == "__main__":
 
     b = int(sys.argv[3])
 
+    operator = sys.argv[2]
+
     if argc < 1:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     else:
-        if sys.argv[2] == '+':
-            print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
+        if operator == '+':
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
 
-        elif sys.argv[2] == '-':
-            print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
-        
-        elif sys.argv[2] == '/':
-            print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
+        elif operator == '-':
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        elif operator == '/':
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
 
-        elif sys.argv[2] == '*':
-            print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
+        elif operator == '*':
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
 
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
-    

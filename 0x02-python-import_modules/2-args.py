a27774bb16c8be4f20f645c23015c72fc98c5a4e
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-14 08:58:33 +0000

    second task 2 attempt

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 import sys
 
-argc = len(sys.argv)
+argc = len(sys.argv) - 1
 
-i = 0
-if argc == 2:
+i = 1
+if argc == 1:
     print("1 argument:")
-    print("1: {}".format(sys.argv))
+    print("1: {}".format(sys.argv[1]))
 
-elif argc > 2:
+elif argc > 1:
     print("{} arguments:".format(argc))
 
-    for argument in sys.argv:
+    for argument in sys.argv[1:]:
         print("{}: {}".format(i, sys.argv[i]))
         i += 1
 

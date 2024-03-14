0955cb7341245e18574f15960c370eddb20ed9c3
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-14 09:58:08 +0000

    task 3 first attempt

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    argc = len(sys.argv) - 1
+
+    result = 0
+
+    i = 1
+
+    if argc == 0:
+        print("0")
+
+    elif argc > 0:
+        for number in sys.argv[1:]:
+            result += int(sys.argv[i])
+            
+            i += 1
+
+        print(result)
+

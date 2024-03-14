bcf981b1a1215b2ab8d378ca4a4b8431c357c769
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-14 11:37:15 +0000

    task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,11 @@
-import marshal
+#!/usr/bin/python3
+import sys
+import hidden_4 as hidden
 
-s = open('hidden_4.pyc')
+if __name__ != "__main__":
+    exit()
 
-s.seek(8)
-
-code_obj = marshal.load(s)
+for name in dir(hidden):
+    if name[0:2] != "__":
+        print(name)
 

2ecdd28837d5998ca179bd3958bfd8adc1a30346
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-14 10:50:11 +0000

    another task 100 attempt

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,36 +5,26 @@ if __name__ == "__main__":
 
     argc = len(sys.argv) - 1
 
-    result = 0
-
-    operator = ""
-
-    a = int(sys.argv[1])
-
-    b = int(sys.argv[3])
-
-    operator = sys.argv[2]
-
     if argc != 3:
         print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
         exit(1)
 
-    if argc < 1:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+
+    if operator == '+':
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+
+    elif operator == '-':
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+
+    elif operator == '/':
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+
+    elif operator == '*':
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
 
     else:
-        if operator == '+':
-            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-
-        elif operator == '-':
-            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-        elif operator == '/':
-            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
-
-        elif operator == '*':
-            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
-
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)

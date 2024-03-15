03cee3057d5e054a5b7d89d65d6eebcabdfa5bd9
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-15 17:12:06 +0000

    task 4 attempt one

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    original_list = []
+    new_list = []
+
+    if idx < 0 and idx > len(my_list) - 1:
+        original_list = my_list
+        return original_list
+    
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
+        

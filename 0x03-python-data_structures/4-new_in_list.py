22400091ea26406cdbd60012828df419a8fcad1c
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-15 17:13:39 +0000

    task 4 fixed pycodestyle

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,9 +6,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0 and idx > len(my_list) - 1:
         original_list = my_list
         return original_list
-    
     else:
         new_list = my_list[:]
         new_list[idx] = element
         return new_list
-        

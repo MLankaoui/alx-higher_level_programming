a76887a10cd18bd05a17b0e63dba6bb2ab4ac7bf
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-16 10:36:20 +0000

    task 0 attempt two

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__": 
-    def print_list_integer(my_list=[]):
-        for number in range(len(my_list)):
-            print(my_list[number])
+def print_list_integer(my_list=[]):
+    for number in range(len(my_list)):
+        print(my_list[number])

5960c294658732aec3d9f538a1a0a96121627213
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-17 14:27:38 +0000

    task 6 chmod

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print('')
         for element in row:
             print("{:d} ".format(element), end='')
+        print('')
 
     print('')
 

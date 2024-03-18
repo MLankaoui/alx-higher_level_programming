9f6cc3aa679c6a5717ed446b095922abce5bf28b
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-18 09:14:58 +0000

    task 8 quick update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     my_tuple = ()
-    if len(sentence) <= 0:
+    if len(sentence) == 0:
         return None
     else:
         my_tuple = (len(sentence), sentence[0])

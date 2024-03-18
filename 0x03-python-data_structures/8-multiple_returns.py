b893928872803986be7d0a78df93892572140f10
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-18 09:30:15 +0000

    fixed pycodestyle fixed and optimised the code task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    sentence_length = len(sentence)
-    my_tuple = (sentence_length, sentence[0])
     if len(sentence) == 0:
-        sentence_length = 0
-        sentence[0] = None
-        return my_tuple
+        my_tuple = (0, None)
     else:
-        return my_tuple
+        my_tuple = (len(sentence), sentence[0])
+
+    return my_tuple

5922e4f1d1031c30a4e9edc8c53e155b902368cf
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-20 11:05:15 +0000

    another attempt

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    index = new_list.index(search)
-    new_list[index] = replace
+    if search in new_list:
+        index = new_list.index(search)
+        new_list[index] = replace
+    else:
+        new_list.append(search)
 
     return new_list

fb928a260c978367ce459e84d3210be0c9f45d32
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-02 14:27:11 +0000

    task 2 first attempt

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return (count)

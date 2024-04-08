43846091fcc597404be4768ecf7870970054071e
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-08 15:47:07 +0000

    task 2 pycodestyle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,7 +49,7 @@ class Rectangle:
 
     def area(self):
         return self.__height * self.__width
- 
+
     def perimeter(self):
         if self.__width == 0 and self.__height == 0:
             return 0

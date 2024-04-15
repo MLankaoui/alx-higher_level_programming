c554d85cb3e3bad0664fc5aef6b90b61f6599091
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-15 15:21:20 +0100

    adding comments to task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
 
+"""a script that creates a function that returns true if the object 
+is an instance of a class that inherited (directly or indirectly) from the specified class 
+otherwise False"""
+
 
 def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherits (directly or indirectly) from the specified class
+    otherwise False.
+    """
     if issubclass(obj, a_class):
         return True
     else:

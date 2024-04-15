b1e133e87dc99283631a16feb6f9d9ae822eb3a9
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-15 11:32:02 +0100

    task 6 adding comments

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -2,7 +2,9 @@
 
 
 """Base Geomety class for Geometries"""
-class BaseGeometry:
 
+
+class BaseGeometry:
+    """public method are not yet implemented"""
     def area(self):
         raise Exception("area() is not implemented")

99f3c3553d4d545870ae3b4e4dc51483c2d2ac9e
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-15 09:35:33 +0100

    task 8 first attempt

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Base Geomety class for Geometries"""
+class BaseGeometry:
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        
+
+"""Rectangle class for drawing a rectangle"""
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+        

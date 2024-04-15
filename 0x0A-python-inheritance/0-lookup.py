5f422b21ada81ff1a97eacd548f504e96ae50bd0
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-15 11:09:45 +0100

    adding comments to task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
+
+"""
+this script defines a function that
+returns a list of attributes of an instance
+"""
 def lookup(obj):
+    """
+    Returns a list of attributes of an instance.
+    """
     return dir(obj)

52decc21bcd8a3dafab0ba79f5e09eba62c50933
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-15 11:24:11 +0100

    task 3 pycodestyle

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 
 
 """this script defines a function that returns true
-if the object is an instance of the given class, or 
+if the object is an instance of the given class, or
 if the object is an instance of a class that inherited from
 otherwise False."""
 
@@ -11,7 +11,7 @@ def is_kind_of_class(obj, a_class):
     """Returns True if
     the object is an instance of the given class, or
     a class that inherits from another one otherwise False."""
-    
+
     if isinstance(obj, a_class):
         return True
     else:

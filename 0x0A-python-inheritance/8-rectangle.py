f0a5718d95c392dc1a27dbd308d82c705ab26830
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-15 20:54:14 +0100

    task 8 pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,13 +9,13 @@ class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
 
-    """a method that checks if the value is an integer and greater than 0"""    
+    """a method that checks if the value is an integer and greater than 0"""
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        
+
 
 """Rectangle class for drawing a rectangle"""
 
@@ -27,4 +27,3 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-        

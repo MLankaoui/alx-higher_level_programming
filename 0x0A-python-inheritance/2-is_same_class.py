6ec9aaab67df89f0a4a65a270d6492b823e0450e
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-15 11:15:15 +0100

    task 2 adding some comments

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 
+
+"""
+this script defines a function that return true
+if th object is an instance of the given class and
+false otherwise
+"""
+
+
 def is_same_class(obj, a_class):
+
+    """return true if obj is an instance of the given class
+    false otherwise"""
     return True if type(obj) is a_class else False
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -20,5 +20,4 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-
         

1e0a926d2f535fcc786957a26f1a9193ceab387d
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-15 20:58:22 +0100

    task 9 adding comments and fixing pycode style

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
 
-"""Base Geomety class for Geometries"""
-class BaseGeometry:
 
+"""Base Geomety class for Geometries"""
+
+
+class BaseGeometry:
+    """a method that raise an exception"""
     def area(self):
         raise Exception("area() is not implemented")
-    
+
+    """a method that checks if the value is an integer and greater than 0"""
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        
+
 
 """Rectangle class for drawing a rectangle"""
+
+
 class Rectangle(BaseGeometry):
+    """initializes a new Rectangle"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
-    def __str__(self) -> str:
+    """defining width / height"""
+    def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
     
+
+    """calculating the erea"""
     def area(self):  # Corrected method name
         return self.__width * self.__height

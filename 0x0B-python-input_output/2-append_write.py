7cfee99325996fb74a74fe0bddc1177e54cfbf77
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-16 10:52:20 +0100

    task 2 first attempt and pycodestyle fixed

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 
-"""a script in python that defines a function the append some text into a text file and return the number of characters written"""
+"""a script in python that defines a function the append
+some text into a text file and return the number of
+characters written"""
 
 
 def append_write(filename="", text=""):
-    """append content from text into filename and return number of characters written"""
-    with open(filename,"a", encoding="utf-8") as f:
+    """append content from text into filename and return number
+    of characters written"""
+    with open(filename, "a", encoding="utf-8") as f:
         """wite returns the number of characters written"""
         return f.write(text)

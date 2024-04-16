011ff9c944c2739663a4cd0ca7161f0df619bf11
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-16 10:37:18 +0100

    task 0 first attempt

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""a python scripts that defines a function that read content from a file
+and prints its content to stdout
+"""
+
+
+def read_file(filename=""):
+
+    """read a file with utf-8 encoding"""
+
+    with open(filename, "r" , encoding='utf-8') as file:
+
+        """stocking the content of the file in content variable"""
+
+        content = file.read()
+
+    """printing content"""
+
+    print(content)
+
+    """closing the file even though it is closed automatically when whe used (with)"""
+
+    file.close()

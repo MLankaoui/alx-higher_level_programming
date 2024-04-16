86309be4b6d4f37835bb9fd24063103dd3f3dbd7
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-16 11:30:58 +0100

    tsk 5 pycodestyle problems fixed

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,6 +3,7 @@
 Object to a text file, using a JSON representation"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """save the JSON representation"""
     string_obj = json.dumps(my_obj)

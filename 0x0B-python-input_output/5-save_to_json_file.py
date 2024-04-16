457ad98b6af3473294d00a01e1c73b72f1c4b7bc
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-16 11:32:02 +0100

    tsk 5 another attempt (hopefully last one)

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """save the JSON representation"""
-    string_obj = json.dumps(my_obj)
     with open(filename, "w", encoding="utf-8") as file:
+        string_obj = json.dumps(my_obj)
         file.write(string_obj)

4bfc1f90f08f920c8ccf2a21d3b534399c3eb8dd
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-16 10:50:37 +0100

    task 1 first attempt and pycodestyle fixed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 
-"""a script in python that defines a function the write some text into a text file and return the number of characters written"""
+"""a script in python that defines a function the write some text
+into a text file and return the number of characters written"""
 
 
 def write_file(filename="", text=""):
-    """overwrite content from text into filename and return number of characters written"""
-    with open(filename,"w", encoding="utf-8") as f:
+    """overwrite content from text into filename and return number
+    of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
         """wite returns the number of characters written"""
         return f.write(text)

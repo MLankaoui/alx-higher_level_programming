51305c5a1dcb29fce446fc6fe1733b9128c7dacf
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-16 11:04:31 +0100

    task 4 first attempt

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a script that define a function that returns an object
+(Python data structure) represented by a JSON string:"""
+import json
+
+
+def from_json_string(my_str):
+    """returns an object (Python data structure) represented by a JSON
+    string"""
+    return json.loads(my_str)

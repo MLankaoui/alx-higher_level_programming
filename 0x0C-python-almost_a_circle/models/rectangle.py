82e5a3f840567074e467edd70353d3d36df6cd32
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-19 13:12:44 +0100

    task 2 second attempt

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,3 +9,34 @@ class Rectangle(Base):
         self.__height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.x
+
+    @x.setter
+    def x(self, value):
+        self.x = value
+
+    @property
+    def y(self):
+        return self.y
+    @y.setter
+    def y(self, value):
+        self.y = value
+

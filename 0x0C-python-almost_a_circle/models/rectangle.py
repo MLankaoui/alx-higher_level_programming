efcda4b041e147b0e4e66c05aae54ce5c3e58438
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-20 16:13:58 +0100

    task 6 first attempt

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,6 +72,9 @@ class Rectangle(Base):
                 print("#", end='')
 
             print()
+
+    def __str__(self):
+        return "[REACTANGLE] ({}) {}/{} - {} / {}".format(self.id, self.__x , self.__y, self.__width, self.__height)
             
 
     

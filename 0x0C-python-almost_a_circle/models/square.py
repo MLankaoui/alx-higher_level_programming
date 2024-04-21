94345d5b61fc5aedc2d504e57714d5c976fed82a
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-21 17:58:17 +0100

    task 11 first attempt

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,4 +11,13 @@ class Square(Rectangle):
 
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
+    
+    @property
+    def size(self):
+        return self.width    
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 

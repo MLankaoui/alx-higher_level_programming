1287e8fff9d12a0c67bf403b919b29f12f2dfc59
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-22 15:56:16 +0100

    square comments added

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,41 +1,46 @@
 #!/usr/bin/python3
-from inspect import classify_class_attrs
+""" Square class """
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    
+    """This class represents a square."""
+
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y,id)
-        self.size = size;
+        """Initialize a Square instance."""
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
-    
+        """Return a string representation of the square."""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
+
     @property
     def size(self):
-        return self.width    
-    
+        """int: The size of the square."""
+        return self.width
+
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        attritbutes = ["id", "size", "x", "y"]
-        if args != None and len(args) != 0:
-            for i in range(len(args)):   
-                setattr(self, attritbutes[i], args[i])
-
+        """Update the square's attributes."""
+        data = ["id", "size", "x", "y"]
+        if args:
+            for i in range(len(args)):
+                setattr(self, data[i], args[i])
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-       
+
     def to_dictionary(self):
+        """Return a dictionary representation of the square."""
         return {
-            "id": self.id, 
+            "id": self.id,
             "size": self.size,
             "x": self.x,
             "y": self.y,
         }
-

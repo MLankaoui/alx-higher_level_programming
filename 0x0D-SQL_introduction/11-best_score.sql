cf844927f03954ebafeb5a912950f8ba7fac3ebd
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-21 19:22:31 +0100

    task 11 another attempt

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- a script that select all the the persons that has a score more than 20 and order in descending order
-SELECT * FROM second_table
+SELECT score, name FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;

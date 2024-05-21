efc3483d1c5818634f6861b101014e97b2fa0910
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-21 20:14:49 +0100

    task 16 4th attempt

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in my MySQL server.
-SELECT score, FROM second_table
+SELECT score, name FROM second_table
 WHERE name != ""
 ORDER BY score DESC;

aefbe17ea843baadd580ad8eb6460afbc6b4abb9
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-21 19:06:19 +0100

    task 11 2th attempt

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
--- script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in my MySQL server.
+-- a script that select all the the persons that has a score more than 20 and order in descending order
 SELECT * FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;

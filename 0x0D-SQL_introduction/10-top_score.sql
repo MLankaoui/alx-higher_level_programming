ebaf49f20b3ee059268e309d59d65798035160f0
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-21 18:52:14 +0100

    task 10 4th attempt

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in my MySQL server.
 SELECT id, name, score FROM second_table
-ORDER BY id ASC, name ASC, score ASC;
+ORDER BY score DESC;

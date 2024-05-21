7128ffdda274a217227a975c9b55fced91af9b17
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-21 16:30:47 +0100

    task 1 third attempt

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,10 +1,3 @@
 -- creating a database
 -- oh i know how to do it!
-IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'hbtn_0c_0')
-BEGIN
-    CREATE database hbtn_0c_0;
-END
-ELSE
-BEGIN
-    PRINT 'DATABASE ALREADY EXISTS';
-END
+CREATE database hbtn_0c_0;

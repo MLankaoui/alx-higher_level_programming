a19e96741f72ffb69793895f87c90c3e062447e9
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-21 17:49:04 +0100

    task 9 6th attempt

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS second_table(
 INSERT INTO second_table (id, name, score)
 VALUES (1, "Jhone", 10);
 INSERT INTO second_table (id, name, score)
-(2, “Alex”, 3);
+VALUES (2, “Alex”, 3);
 INSERT INTO second_table (id, name, score)   
-(3, “Bob”, 14);
+VALUES (3, “Bob”, 14);
 INSERT INTO second_table (id, name, score)
-(4, “George”, 8);
+VALUES (4, “George”, 8);

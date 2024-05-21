10a4b35cf7b368313ee41a15a88b5b22cb960bba
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-21 16:59:38 +0100

    task 4 10th attempt

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
---creating a table in the current database
+-- script that creates a table called first_table in the current database in MySQL server
 CREATE TABLE `first_table`(`id` INT, `name` VARCHAR(256));

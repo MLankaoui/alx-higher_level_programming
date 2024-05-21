b7f182aef0b8f4bf2742e23d75a651a11f78763c
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-21 19:13:30 +0100

    task 9 another attempt

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- script that creates a table second_table in the database hbtn_0c_0 in my MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table(
     id INT,
-    name VARCHAR(50),
+    name VARCHAR(256),
     score INT
 );
 

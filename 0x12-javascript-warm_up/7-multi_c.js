2ecc212fb0927a8d3cbd92abe8d7fd30f9a41d4c
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-18 14:48:06 +0100

    task 7 semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 const process = require('process');
 
-let argv = process.argv;
+const argv = process.argv;
 
-let argc = argv.length - 2;
-let number = parseInt(argv[2]);
+const argc = argv.length - 2;
+const number = parseInt(argv[2]);
 
 if (isNaN(number)) {
-        console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
 
 if (argc > 0 && !isNaN(number)) {
-    let idx = 0;
-    for (; idx < number ; idx++) {
-        console.log('C is fun');
-    }
+  let idx = 0;
+  for (; idx < number; idx++) {
+    console.log('C is fun');
+  }
 }

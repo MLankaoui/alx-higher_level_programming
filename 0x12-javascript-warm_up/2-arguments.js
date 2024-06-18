421fcb91496062efdbddd72bf9ece67f1477e589
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-18 12:01:21 +0100

    task 2 first attempt

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// including the process module
+const process = require('process');
+let argc = process.argv.length - 2;
+
+if (argc == 0)
+{
+    console.log("No argument");
+}
+
+else if (argc == 1)
+{
+    console.log('Argument found');
+}
+
+else if (argc > 1)
+{
+    console.log('Arguments found')    
+}    

ca8b5b098d9d58e5aa06e314cc8be63f779e9670
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-18 16:46:13 +0100

    task 9 first attempt

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,16 +5,17 @@ let argv = process.argv;
 
 let argc = argv.length - 2;
 
-
-console.log(argc);
-function add(argv, argc) {
-    if (argc != 2) {
-        console.log(NaN);
-    }
-
-    else {
-        console.log(parseInt(argv[2]) + parseInt(argv[3]));
-    }
+if (argc != 2) {
+    console.log(NaN);
 }
 
-add(argc, argv);
+else {
+    let number_one = parseInt(argv[2]);
+    let number_two = parseInt(argv[3]);
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    console.log(add(number_one, number_two));
+}

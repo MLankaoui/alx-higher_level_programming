f67f985b36173be773e7690e99782160a355f397
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-18 15:39:59 +0100

    task 8 second attempt

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -12,15 +12,19 @@ if (isNaN(number)) {
 }
 
 if (argc > 0 && !isNaN(number)) {
-    let i, j;
-
-    i = 0;
-    j = 0;
-    for (; i < number; i++) {
-        for (; j < number; j++) {
-            console.log("x".repeat(number));  
-        }
-
-        console.log("x")
-    }
+   
+   let idx = 0;
+   for (; idx < number; idx++) {
+    console.log("x".repeat(number));
+   } 
 }
+
+// let i, j;
+
+//     i = 0;
+//     j = 0;
+//     for (; i < number; i++) {
+//         for (; j < number; j++) {
+//             console.log("x".repeat(number));  
+//         {
+//     }

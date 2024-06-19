404d611a7bb7ed3f393a9878639a85ce020ac865
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-19 13:08:48 +0100

    task 3 semistandard

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-      // create an empty object if the w or h ar not postivie
-      if (!(w > 0 && h > 0)) {
-        return;
-      }
-      this.width = w;
-      this.height = h;
-    
+  constructor (w, h) {
+    // create an empty object if the w or h ar not postivie
+    if (!(w > 0 && h > 0)) {
+      return;
     }
-    print() {
-        let i;
-        for (i = 0; i < this.height; i++) {
-            console.log("X".repeat(this.width));
-        }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let i;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
-    
-  };
+  }
+};

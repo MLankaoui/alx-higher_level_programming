a4e5cb92a59ee2669454a238848b0d714c3e5888
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-23 12:35:51 +0100

    task 2 first attempt

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 5:
+        exit(1)
+
+    username, password, database, statename = argv[1], argv[2], argv[3], argv[4]
+    db = MySQLdb.connect(host='localhost', port=3305, user=username, passwd=password, db=database, charset='utf8')
+
+    cursor = db.cursor()
+
+    cursor.execute('SELECT * FROM states WHERE name=%s', (statename,))
+
+    query_r = cursor.fetchall()
+
+    print(query_r)
+
+    cursor.close()
+    db.close()
